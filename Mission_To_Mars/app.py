e797ffd72bb18920028fd55de3d66ca9814eb86e
KayCan <cannon.kaycee@gmail.com>
2022-05-23 14:34:32 -0700

    second

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -6,17 +6,21 @@ import pymongo
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-# Create connection variable
-conn = 'mongodb://localhost:27017'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+mongo = PyMongo(app)
 
 # Set route
 @app.route('/')
 def home():
-    return render_template('index.html', )
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
 
-@app.route('/scrape')
-def scrape():
-    return render_template('index.html', )
+@app.route("/scrape")
+def mars_scrape():
+    listings = mongo.db.listings
+    listings_data = scraped_data.scrape()
+    listings.update_one({}, {"$set": listings_data}, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
